93914c084a5c88273c43d2439e8af62946078bcf
JUNHYUNG CHO <junhyungcho@JUNHYUNGui-MacBookPro.local>
2022-10-05 13:15:30 +0900

    리뷰 반영

--- db/migrations/20221004122435_create_regions_table.sql
+++
@@ -1,8 +0,0 @@
--- migrate:up
-CREATE TABLE regions (
-    id int not null auto_increment primary key,
-    name varchar(100) not null UNIQUE
-)
-
--- migrate:down
-DROP TABLE regions;
--- db/migrations/20221004122859_create_airlines_table.sql
+++
@@ -1,9 +0,0 @@
--- migrate:up
-CREATE TABLE airlines (
-    id int not null auto_increment primary key,
-    name varchar(100) not null UNIQUE,
-    image varchar(1000) not null
-)
-
--- migrate:down
-DROP TABLE airlines;
